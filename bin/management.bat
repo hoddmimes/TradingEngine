7e7f3cc98b4140d05d64fadd5e4ad7d6cf8c3008
pobe <par.bertilsson@hoddmimes.com>
2022-03-28 16:27:55 +0200

    cut & paste

--- bin/management.bat
+++ bin/management.bat
@@ -1,4 +1,5 @@
-@echo off
+@echo on
+
 
 set BINDIR="%~dp0"
 call %BINDIR%/version.bat
@@ -6,7 +7,7 @@ call %BINDIR%/version.bat
 rem -- push current location
 pushd "%cd%"
 rem -- set default to the TE working dir
-cd %BINDIR%/..
+cd %BINDIR%..
 SET JAVA-SWITCHES=-Dlog4j.configurationFile=./configuration/log4j2-te.xml -Djava.net.preferIPv4Stack=true
 java %JAVA-SWITCHES% -cp ./meta-engine/build/libs/te-%TE_VERSION%.jar  com.hoddmimes.te.management.gui.mgmt.Management -grpAddr 224.20.20.20 -grpPort 3939
 popd
